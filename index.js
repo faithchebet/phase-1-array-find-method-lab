2713237bf256feae4d6730e36497d626b23fbe84
faithchebet <faith.chebet@student.moringaschool.com>
2023-12-20 18:36:39 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function superbowlWin(arrayOfObjects) {
+    const winningYearObject = arrayOfObjects.find(obj => obj.result === "W");
+  
+    if (winningYearObject) {
+      return winningYearObject.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const superbowls = [
+    { year: 2012, result: "L" },
+    { year: 2011, result: "W" },
+    { year: 2010, result: "L" },
+
+  ];
+  
+  const winningYear = superbowlWin(superbowls);
+  console.log(winningYear); 
